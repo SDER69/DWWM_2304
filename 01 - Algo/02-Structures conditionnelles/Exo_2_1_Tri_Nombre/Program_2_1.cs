3390e9152e920a6855444b751b46c93c4726de29
SDER69 <131238935+SDER69@users.noreply.github.com>
2023-04-27 08:41:11 +0200

    Mise à jour exercices

---
+++ 01 - Algo/02-Structures conditionnelles/Exo_2_1_Tri_Nombre/Program_2_1.cs
@@ -0,0 +1,29 @@
+﻿namespace Tri_de_nombres
+{
+    internal class Program_2_1
+    {
+        static void Main(string[] args)
+        {
+            int a, b;
+
+            Console.WriteLine("Veuillez entrer un premier nombre a : ");
+            a = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Veuillez entrer un premier nombre b : ");
+            b = int.Parse(Console.ReadLine());
+
+            if (a>b)
+            {
+                Console.WriteLine("a = "+a+" b = "+b);
+            }
+            else
+            {
+                Console.WriteLine("b = " + b+ " a = " + a);
+            }
+
+
+
+
+        }
+    }
+}
